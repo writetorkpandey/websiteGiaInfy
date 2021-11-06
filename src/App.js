aec2a476bcd5891d16815b02af5d094d02683c31
unknown <amitbecs08@gmail.com>
2021-11-06 11:22:45 +0530

    Activity page modified

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 
 import logo from "./logo.svg";
 import "./App.css";
-import Header from "./Components/Header";
-import { Footer } from "./Components/Footer";
-import Aboutus from "./Components/Aboutus";
-import Media from "./Components/Media";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Homepage from "./Pages/Homepage";
 import Aboutuspage from "./Pages/Aboutuspage";
 import Mediapage from "./Pages/Mediapage";
-
+import ActivityPage from "./Pages/ActivityPage";
+import Contactuspage from "./Pages/Contactuspage";
 
 function App() {
   return (
@@ -20,6 +17,8 @@ function App() {
           <Route path="/" exact component={Homepage} />
           <Route path="/Aboutuspage" exact component={Aboutuspage} />
           <Route path="/Mediapage" component={Mediapage} />
+          <Route path="/Activitypage" component={ActivityPage} />
+          <Route path="/Contactuspage" component={Contactuspage} />
         </Switch>
       </Router>
     </div>
---
+++ src/Pages/ActivityPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Footer } from "../Components/Footer";
+import Header from "../Components/Header";
+import Activity from "../Components/Activity";
+
+export default function ActivityPage() {
+  return (
+    <>
+      <Header />
+      <Activity/>
+      <Footer />
+    </>
+  );
+}
