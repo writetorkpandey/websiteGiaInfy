eec40eb7492e01866f8bb92f33899176b8422443
Rahul <60106658+writetorkpandey@users.noreply.github.com>
2021-11-07 07:51:08 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,77 +10,57 @@ export default function Header() {
     <div>
       {/* Start of NavBar Code  */}
 
-      <nav class="navbar navbar-expand-lg navbar-light fbg">
-        <div class="container">
-          <div
-            class="collapse navbar-collapse row align-items-start "
-            id="navbarNav"
-          >
-            <ul class="navbar-nav col">
-              <li class="nav-item">
-                <a class=" sname Thover" href="#">
-                  Gramarshi Academy International
-                </a>
-              </li>
-            </ul>
+     <section className="navbar-bg">
+        <nav class="navbar navbar-expand-lg navbar-light ">
+          <div class="container">
+            <a class="navbar-brand" href="#"></a>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+              onClick={() => setShow(!show)}
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class={`collapse navbar-collapse ${show ? "show" : ""}`}>
+              <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <NavLink class="nav-link active" aria-current="page" to="/">
+                    Home
+                  </NavLink>
+                </li>
+                <li class="nav-item">
+                  <NavLink class="nav-link" to="/service">
+                    School Founders
+                  </NavLink>
+                </li>
+                <li class="nav-item">
+                  <NavLink class="nav-link" to="/about">
+                    About
+                  </NavLink>
+                </li>
+                <li class="nav-item">
+                  <NavLink class="nav-link" to="/contact">
+                    Contact
+                  </NavLink>
+                </li>
+              </ul>
+              <form class="d-flex">
+                {/* <button class="btn  btn-style" type="submit">
+                  Sign Up
+                </button> */}
+                <button class="btn  btn-style btn-style-border" type="submit">
+                  Admin Login
+                </button>
+              </form>
+            </div>
           </div>
-
-          <div class="row align-items-end" id="navbarNav">
-            <ul class="navbar-nav col">
-              <li class="nav-item">
-                <Link to="/" class="nav-link Thover">
-                  HOME
-                </Link>
-              </li>
-
-              <li class="nav-item Thover">
-                <Link to="/Aboutuspage" class="nav-link">
-                  ABOUT US
-                </Link>
-              </li>
-
-              <li class="nav-item Thover">
-                <a class="nav-link" href="#">
-                  ACTIVITIES
-                </a>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link Thover" href="#">
-                  ADMISSION
-                </a>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link Thover" href="#">
-                  ACHIEVEMENTS
-                </a>
-              </li>
-
-              <li class="nav-item">
-                <Link to="/Mediapage" class="nav-link Thover">
-                  MEDIA
-                </Link>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link Thover" href="#">
-                  CONTACT US
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
-      {/* End of NavBar Code */}
-
-      {/* Data below Navbar */}
-      <section className="scetion">
-        <div className="container"></div>
+        </nav>
       </section>
-
-      {/* Data post navbar  */}
     </div>
   );
 }
