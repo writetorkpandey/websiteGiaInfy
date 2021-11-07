e83e152584d09da5eaa36c97cb3ba080dc626e43
Rahul <writetorkpandey@gmail.com>
2021-11-07 08:51:27 +0530

    updated header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,66 +1,72 @@
-
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-
 export default function Header() {
-
-
+  const [show, setShow] = useState(false);
   return (
-    <div>
-      {/* Start of NavBar Code  */}
+    <>
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-primary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            Gramarshi Academy International School
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarCollapse"
+            aria-controls="navbarCollapse"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+            onClick={() => setShow(!show)}
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class={`collapse navbar-collapse ${show ? "show" : ""}`}>
+            <ul class="navbar-nav me-auto mb-2 mb-md-0">
+              <li class="nav-item">
+                <Link to="/" class="nav-link ">
+                  HOME
+                </Link>
+              </li>
+              <li class="nav-item ">
+                <Link to="/Aboutuspage" class="nav-link">
+                  ABOUT US
+                </Link>
+              </li>
+              <li class="nav-item ">
+                <Link class="nav-link" href="#">
+                  ACTIVITIES
+                </Link>
+              </li>
+              <li class="nav-item ">
+                <Link class="nav-link" href="#">
+                  ADMISSION
+                </Link>
+              </li>
+              <li class="nav-item ">
+                <Link class="nav-link" href="#">
+                  ACHIEVEMENTS
+                </Link>
+              </li>
+              <li class="nav-item ">
+                <Link class="nav-link" href="#">
+                  MEDIA
+                </Link>
+              </li>
+              <li class="nav-item ">
+                <Link class="nav-link" href="#">
+                  CONTACT US
+                </Link>
+              </li>
+            </ul>
 
-     <section className="navbar-bg">
-        <nav class="navbar navbar-expand-lg navbar-light ">
-          <div class="container">
-            <a class="navbar-brand" href="#"></a>
-            <button
-              class="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-              onClick={() => setShow(!show)}
-            >
-              <span class="navbar-toggler-icon"></span>
+            <button class="btn btn-outline-success" type="button">
+              Login
             </button>
-            <div class={`collapse navbar-collapse ${show ? "show" : ""}`}>
-              <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <NavLink class="nav-link active" aria-current="page" to="/">
-                    Home
-                  </NavLink>
-                </li>
-                <li class="nav-item">
-                  <NavLink class="nav-link" to="/service">
-                    School Founders
-                  </NavLink>
-                </li>
-                <li class="nav-item">
-                  <NavLink class="nav-link" to="/about">
-                    About
-                  </NavLink>
-                </li>
-                <li class="nav-item">
-                  <NavLink class="nav-link" to="/contact">
-                    Contact
-                  </NavLink>
-                </li>
-              </ul>
-              <form class="d-flex">
-                {/* <button class="btn  btn-style" type="submit">
-                  Sign Up
-                </button> */}
-                <button class="btn  btn-style btn-style-border" type="submit">
-                  Admin Login
-                </button>
-              </form>
-            </div>
           </div>
-        </nav>
-      </section>
-    </div>
+        </div>
+      </nav>
+    </>
   );
 }
