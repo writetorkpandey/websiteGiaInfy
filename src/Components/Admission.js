272e15b5cb265b7902f6d85d0b3cc8b18e5525dd
unknown <amitbecs08@gmail.com>
2021-11-18 08:13:10 +0530

    Admission

---
+++ src/Components/Admission.js
@@ -0,0 +1,33 @@
+import React,{useState,useEffect} from 'react'
+import db from "../firebaseConfig";
+import { onSnapshot, collection } from "firebase/firestore";
+
+export default function Admission() {
+
+    const [admissionData, setAdmissionData] = useState([]);
+
+    useEffect(
+      () =>
+        onSnapshot(collection(db, "admission"), (snapshot) =>
+          {setAdmissionData(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+        console.log(admissionData)}
+        ),
+      []
+    );
+
+return(
+    <div className="container mt-5">
+        <h3 className="text-success display-6">Please read below admission process in detail.</h3>
+        <br/>
+    {admissionData.map((item)=>{
+        return (
+            <div>
+                <h5 className="h5 text-primary text-start">{item.point}</h5>
+                <p className="text-secondary text-start">{item.description}</p>
+                <hr/>
+            </div>
+        )
+    })}
+    </div>
+);
+}
---
+++ src/Pages/AdmissionPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Admission from "../Components/Admission";
+import { Footer } from "../Components/Footer";
+import Header from "../Components/Header";
+
+export default function AdmissionPage() {
+  return (
+    <>
+      <Header />
+      <Admission />
+      <Footer />
+    </>
+  );
+}
