4f333419c06a8a530dc85d1a290de05086e9ff95
Rahul <writetorkpandey@gmail.com>
2021-12-04 12:51:33 +0530

    Updates

---
+++ precache-manifest.52cc3645f51fb6ecb87215ef3b718121.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc7c5cb04811225a5f41699ea0666de9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9a44cd069d928106764",
+    "url": "/static/css/main.c01a73e5.chunk.css"
+  },
+  {
+    "revision": "2532b5a24bb26fc40cc6",
+    "url": "/static/js/2.fdbc89b2.chunk.js"
+  },
+  {
+    "revision": "c70531564bb98a3348eff57ed7b638e6",
+    "url": "/static/js/2.fdbc89b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4507b403f81aa5ebbb49",
+    "url": "/static/js/3.0ab26c6a.chunk.js"
+  },
+  {
+    "revision": "c4d2f96e9c98a0085c14",
+    "url": "/static/js/4.1a96c7ca.chunk.js"
+  },
+  {
+    "revision": "e9a44cd069d928106764",
+    "url": "/static/js/main.d3849a61.chunk.js"
+  },
+  {
+    "revision": "b1ab57af9fec21f13988",
+    "url": "/static/js/runtime-main.a8357ff1.js"
+  }
+]);
